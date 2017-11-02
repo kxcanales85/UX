186ca7085eae4aa6cc7a2c01754562c524e6accf
FernandoJHO <fernando.handal@yahoo.com>
2017-11-02 17:32:15 -0300

    boton

--- UNIFIT/app/src/main/java/com/example/bustamante/unifit/MainActivity.java
+++ UNIFIT/app/src/main/java/com/example/bustamante/unifit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bustamante.unifit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,7 +44,23 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Button boton;
+        boton = (Button)findViewById(R.id.btn1);
 
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+                /*AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                builder.setMessage("Hola mundo")
+                        .setTitle("Saludo");
+
+                AlertDialog alerta = builder.create();
+
+                alerta.show();*/
+            }
+        });
     }
 
 }
