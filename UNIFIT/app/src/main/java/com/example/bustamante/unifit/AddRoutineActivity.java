cb020b9517a7362f3418f5679b4081c7026e3432
FernandoJHO <fernando.handal@yahoo.com>
2017-11-08 01:10:18 -0300

    Activity lista de rutinas por músculo y activity agregar rutina

---
+++ UNIFIT/app/src/main/java/com/example/bustamante/unifit/AddRoutineActivity.java
@@ -0,0 +1,116 @@
+package com.example.bustamante.unifit;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class AddRoutineActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private static final int MENU_ITEM_ITEM1 = 0;
+    Button rutina;
+    ImageView image;
+    Spinner series;
+    Spinner repeticiones;
+    Spinner peso;
+    MenuItem item;
+    Intent intent;
+    String nombre_rutina;
+    Integer[] n_series;
+    Integer[] n_repeticiones;
+    Integer[] pesos;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_rutinas:
+                    mTextMessage.setText(R.string.title_rutinas);
+                    return true;
+                case R.id.navigation_inicio:
+                    mTextMessage.setText(R.string.title_inicio);
+                    return true;
+                case R.id.navigation_alimentacion:
+                    mTextMessage.setText(R.string.title_alimentacion);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_routine);
+        setTitle("Agregar rutina");
+
+        setRoutineName();
+
+        setRoutineImage();
+
+        setSpinners();
+    }
+
+    public boolean setRoutineName(){
+        intent = getIntent();
+        nombre_rutina = intent.getStringExtra("nombre");
+        rutina = (Button)findViewById(R.id.routineName);
+        rutina.setText(nombre_rutina);
+        return true;
+    }
+
+    public boolean setRoutineImage(){
+        image = (ImageView)findViewById(R.id.routineImage);
+        image.setImageResource(R.mipmap.flexiones);
+        return true;
+    }
+
+    public boolean setSpinners(){
+        series = (Spinner)findViewById(R.id.seriesSpinner);
+        n_series = new Integer[]{5,10,15,20};
+        series.setAdapter(new ArrayAdapter<Integer>(this,R.layout.spinner_item,n_series));
+
+        repeticiones = (Spinner)findViewById(R.id.repeticionesSpinner);
+        n_repeticiones = new Integer[]{10,15,20,25,30};
+        repeticiones.setAdapter(new ArrayAdapter<Integer>(this,R.layout.spinner_item,n_repeticiones));
+
+        peso = (Spinner)findViewById(R.id.pesoSpinner);
+        pesos = new Integer[]{20,25,30,35,40};
+        peso.setAdapter(new ArrayAdapter<Integer>(this,R.layout.spinner_item,pesos));
+
+        return true;
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation,menu);
+        item = menu.add(Menu.NONE, MENU_ITEM_ITEM1, Menu.NONE, "Agregar");
+        item.setIcon(android.R.drawable.ic_input_add);
+        item.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        return true;
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_ITEM_ITEM1:
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+}
---
+++ UNIFIT/app/src/main/java/com/example/bustamante/unifit/AddRoutinesActivity.java
@@ -0,0 +1,158 @@
+package com.example.bustamante.unifit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddRoutinesActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    Button musculo;
+    TabHost tab;
+    TabHost.TabSpec spec;
+    ListView lista_pormusculo;
+    ArrayList<String> rutinas;
+    MyAdapter adaptador;
+    Intent intent;
+    String nombre_rutina;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_rutinas:
+                    mTextMessage.setText(R.string.title_rutinas);
+                    return true;
+                case R.id.navigation_inicio:
+                    mTextMessage.setText(R.string.title_inicio);
+                    return true;
+                case R.id.navigation_alimentacion:
+                    mTextMessage.setText(R.string.title_alimentacion);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_routines);
+        setTitle("Rutinas");
+
+        setSelectedMuscle();
+
+        setTabs();
+
+        setListView();
+    }
+
+    public boolean setSelectedMuscle(){
+        musculo = (Button) findViewById(R.id.selectedMuscle);
+        musculo.setText("Músculo seleccionado: Biceps");
+
+        return true;
+    }
+
+    public boolean setTabs(){
+        tab = (TabHost)findViewById(R.id.tabhost);
+        tab.setup();
+
+        spec = tab.newTabSpec("Por Nombre");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Por nombre");
+        tab.addTab(spec);
+
+        spec = tab.newTabSpec("Por músculo");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Por músculo");
+        tab.addTab(spec);
+
+        return true;
+    }
+
+    public boolean setListView(){
+        rutinas = new ArrayList<>();
+        //ArrayAdapter<String> adaptador;
+        lista_pormusculo = (ListView)findViewById(R.id.listViewMusculo);
+        //adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        rutinas.add("Flexiones de brazo");
+        rutinas.add("Fondos");
+        rutinas.add("Jalones");
+        rutinas.add("Lagartijas");
+        rutinas.add("Press en banca");
+        adaptador = new MyAdapter(this,R.layout.custom_row,rutinas);
+        lista_pormusculo.setAdapter(adaptador);
+        lista_pormusculo.setClickable(true);
+        lista_pormusculo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                switch (i){
+                    case 0:
+                        intent = new Intent(AddRoutinesActivity.this,AddRoutineActivity.class);
+                        nombre_rutina = adapterView.getItemAtPosition(i).toString();
+                        intent.putExtra("nombre",nombre_rutina);
+                        startActivity(intent);
+                }
+
+            }
+        });
+
+        return true;
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation,menu);
+
+        return true;
+    }
+
+    private class MyAdapter extends ArrayAdapter<String> {
+
+        public MyAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+        }
+
+        public View getView(int position, @Nullable View fila, @NonNull ViewGroup parent) {
+
+            LayoutInflater mInflater = (LayoutInflater) getContext()
+                    .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+            if (fila == null){
+                // Instanciar la fila
+                fila = mInflater.inflate(R.layout.custom_row, null);
+            }
+
+            TextView rutina_view = (TextView) fila.findViewById(R.id.routine);
+
+            String rutina = getItem(position);
+            rutina_view.setText(rutina);
+
+            return fila;
+        }
+    }
+}
