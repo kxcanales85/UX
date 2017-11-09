05eec1c2d1b7fcfb4ced74e86cae378ee5e68500
bustamante <kevin.canales@usach.cl>
2017-11-09 11:57:20 -0300

    Change color and add chart

---
+++ UNIFIT/app/src/androidTest/java/com/example/bustamante/unifit/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.bustamante.unifit;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bustamante on 09-11-17.
+ */
+public class MainActivityTest {
+
+}
--- UNIFIT/app/src/main/java/com/example/bustamante/unifit/MainActivity.java
+++ UNIFIT/app/src/main/java/com/example/bustamante/unifit/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bustamante.unifit;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -9,11 +10,42 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.listener.PieChartOnValueSelectListener;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.LineChartView;
+import lecho.lib.hellocharts.view.PieChartView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private PieChartView pieChart;
+    private PieChartData data;
+    private boolean hasLabels = false;
+    private boolean hasLabelsOutside = false;
+    private boolean hasLabelForSelected = false;
+    private double carbo = 25.9, proteinas = 20.1, grasas = 30.8, total = 0.0;
+
+    private class ValueTouchListener implements PieChartOnValueSelectListener {
+
+        @Override
+        public void onValueSelected(int arcIndex, SliceValue value) {
+            Toast.makeText(MainActivity.this, value.getValue() + " %", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onValueDeselected() {
+        }
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,6 +68,48 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+    private void toggleLabels() {
+        hasLabels = !hasLabels;
+        if (hasLabels) {
+            hasLabelForSelected = false;
+            pieChart.setValueSelectionEnabled(hasLabelForSelected);
+            if (hasLabelsOutside) {
+                pieChart.setCircleFillRatio(0.7f);
+            } else {
+                pieChart.setCircleFillRatio(1.0f);
+            }
+        }
+        generateData();
+    }
+
+    /*Reinicia los valores de las variables*/
+    public void resetCounts() {
+        carbo = 0;
+        proteinas = 0;
+        grasas = 0;
+    }
+
+    private void generateData() {
+        int numValues = 3;	// Numero de particiones y/o variables
+        List<SliceValue> values = new ArrayList<SliceValue>();
+	/*Definimos el tamano (mediante un valor porcentual referente a cierta variable) y el color que tendra*/
+        if (carbo > 0) {
+            SliceValue sliceValueBuenas = new SliceValue((float) ((float) carbo * 100 / total), getResources().getColor(R.color.colorCarbo));
+            values.add(sliceValueBuenas);
+        }
+        if (proteinas > 0) {
+            SliceValue sliceValueMalas = new SliceValue((float) ((float) proteinas * 100 / total), getResources().getColor(R.color.colorProteina));
+            values.add(sliceValueMalas);
+        }
+        if (grasas > 0) {
+            SliceValue sliceValueNulas = new SliceValue((float) ((float) grasas * 100 / total), getResources().getColor(R.color.colorGrasa));
+            values.add(sliceValueNulas);
+        }
+        data = new PieChartData(values);
+        data.setHasLabels(hasLabels); // Muesta el valor de la particion
+        pieChart.setPieChartData(data);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +120,17 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         Button boton;
         boton = (Button)findViewById(R.id.btn1);
+
+        pieChart = (PieChartView) findViewById(R.id.chart);
+
+        carbo = 25.9;
+        proteinas = 20.1;
+        grasas = 30.8;
+        total=carbo+proteinas+grasas;
+
+        pieChart.setOnValueTouchListener(new ValueTouchListener());
+        toggleLabels();
+        generateData();
 
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
